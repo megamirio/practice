c87cb6779ef2b879d1a9ac14120f022b81ae8ba1
stingion <stingion@gmail.com>
2014-12-19 10:40:27 +0200

    Classloading and initialization experiments

--- practice1/src/main/java/stingion/classloading/Starter.java
+++ practice1/src/main/java/stingion/classloading/Starter.java
@@ -5,10 +5,113 @@ import com.sun.java.accessibility.util.Translator;
 /**
  * Created by imalysh on 12/17/2014.
  */
+class Opa {
+
+    static final String str="s";
+
+    static {
+        System.out.println("static block of Opa");
+    }
+
+    {
+        System.out.println("instance of Opa");
+    }
+
+    public Opa(String str) {
+        System.out.println(str);
+    }
+
+    public Opa() {
+    }
+}
+
+class A1 {
+    static {
+        new Opa("a1");
+    }
+}
+
+class A2 extends A1 {
+    static {
+        new Opa("a2");
+    }
+}
+
+interface Ita {
+    Opa strNF = new Opa("ita");
+}
+
+interface ItaAfter extends Ita {
+    Opa strNF2 = new Opa("itaAfter");
+}
+
+class Ok1 implements Ita {
+
+    static {
+        System.out.println("ommm before");
+    }
+
+    private void one() {
+    }
+}
+
+class Ok2 extends Ok1 {
+    static {
+        System.out.println("ommm");
+    }
+
+    public static final String str = "oxy";
+
+    public void one() {
+    }
+}
+
+class A0 {
+
+    public int j = 101;
+
+    public A0() {
+        forOverriding();
+    }
+
+    public void forOverriding() {
+    }
+}
+
+class A extends A0 {
+
+    public int i = 10;
+
+    {
+        System.out.println("A block " + i);
+    }
+
+    public A() {
+        System.out.println("A constructor");
+        forOverriding();
+    }
+
+    @Override
+    public void forOverriding() {
+        System.out.println("It's i = " + i);
+    }
+}
+
 public class Starter {
-    public static void main(String[] args) {
-        System.out.println(Starter.class.getClassLoader());
-        System.out.println(Translator.class.getClassLoader());
-        System.out.println(Object.class.getClassLoader());
+    public static void main(String[] args) throws Exception {
+//        String str = Ok2.str;
+//        System.out.println(str);
+////        System.out.println(Ok2.strNF);
+//        Class.forName("stingion.classloading.Ita");
+//        Class.forName("stingion.classloading.A2");
+////        assert str == null : new Object().getClass();
+////        Class.forName("stingion.classloading.Ok2", true, Ok2.class.getClassLoader());
+//        System.out.println(Starter.class.getClassLoader());
+//        System.out.println(Translator.class.getClassLoader());
+//        System.out.println(Object.class.getClassLoader());
+        System.out.println("---------------------------------------------");
+        Class opaClass = Class.forName("stingion.classloading.Opa", false, Opa.class.getClassLoader());
+        System.out.println(opaClass.getDeclaredConstructors()[0]);
+        System.out.println(Opa.str);
     }
 }
