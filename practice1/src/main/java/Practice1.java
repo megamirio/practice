5ae23bc1c0e46c80cac2c159447648686963157c
stingion <stingion@gmail.com>
2015-10-30 11:30:24 +0200

    Probe with String.format method

--- practice1/src/main/java/Practice1.java
+++ practice1/src/main/java/Practice1.java
@@ -2,4 +2,8 @@
  * Created by ievgen on 8/8/14 9:19 AM.
  */
 public class Practice1 {
+    public static void main(String[] args) {
+        String str = String.format("ABC %s = %s", "abc", 22);
+        System.out.println(str);
+    }
 }
