3eb919a91f17061d76ac240d1c9c7119237eaffc
stingion <stingion@gmail.com>
2015-12-17 16:42:35 +0200

    Java 8 Steams learning

--- practice3/src/main/java/practice3/Streamer.java
+++ practice3/src/main/java/practice3/Streamer.java
@@ -3,8 +3,10 @@ package practice3;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
@@ -12,7 +14,7 @@ import java.util.stream.Collectors;
  */
 public class Streamer {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(3, 3, -5, 555, 23, 5);
+        List<Integer> list = Arrays.asList(3, 3, -5, 555, 23, 5, 2);
         List<Integer> listNew = list.stream().limit(list.size()).filter(num -> num != 3).collect(Collectors.toList());
         System.out.println(list);
         System.out.println(listNew);
@@ -23,5 +25,9 @@ public class Streamer {
                         return o1 < o2 ? -1 : (o1 > o2 ? 1 : 0);
                     }
                 }).forEach(System.out::println);
+        System.out.println("-------------");
+        listNew.stream().map(i -> i * i % 2).distinct().collect(Collectors.toList()).forEach(System.out::println);
+        IntSummaryStatistics stats = list.stream().mapToInt(x -> x).summaryStatistics();
+        System.out.println(stats.getMin());
     }
 }
