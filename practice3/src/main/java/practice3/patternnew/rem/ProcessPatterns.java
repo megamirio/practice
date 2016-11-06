c1af66b0465cd48af59f20872ba56e1a7c6cee6e
Stingion <stingion@gmail.com>
2016-11-06 12:02:02 +0200

    DP studying

--- practice3/src/main/java/practice3/patternnew/rem/ProcessPatterns.java
+++ practice3/src/main/java/practice3/patternnew/rem/ProcessPatterns.java
@@ -56,7 +56,7 @@ public class ProcessPatterns {
 
     public static List<Integer> donePatterns() {
         return Arrays.asList(300, 200, 301, 201, 102, 100, 101, 104, 307, 207, 305, 203, 311, 204, 308, 202, 205, 304,
-                303, 206, 309);
+                303, 206, 309, 103, 105, 106, 107);
     }
 
     public static void main(String[] args) {
